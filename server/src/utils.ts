e54d407802b857085897f4928a84bfc6bb61063a
Femi Ajayi <femiajayi705@gmail.com>
2023-02-20 20:29:24 +0100

    refac: Removed etherscan dependency

--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,30 +1,43 @@
 
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
 import { ethers } from "ethers";
-import EthDater = require("ethereum-block-by-date");
-import * as dotenv from "dotenv";
-dotenv.config();
 
-const etherscanAPIKey = process.env.ETHERSCAN_API_KEY;
 
-interface BlockResponse { date: string, block: number, timestamp: number }
+export const getBlockByTimeStamp: (timestamp: number) => Promise<any> = async (timestamp: number) => {
 
-export const getBlockByTimeStamp: (timestamp: number) => Promise<BlockResponse> = (timestamp: number) => new Promise<BlockResponse>((resolve, reject) => {
-    const provider: any = new ethers.EtherscanProvider("homestead", etherscanAPIKey);
-    const dater = new EthDater(
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-        provider
-    );
-    const date = new Date(timestamp);
     try {
-        const block = dater.getDate(
-            date, // Date, required. Any valid moment.js value: string, milliseconds, Date() object, moment() object.
-            true // Block after, optional. Search for the nearest block before or after the given date. By default true.
-        );
-        resolve(block);
+        // Initialize variables
+        // A block takes approximately 12 seconds
+        const provider: any = new ethers.JsonRpcProvider();
+        const averageBlockTime: number = 12
+        const startBlockNumber: number = await provider.getBlockNumber();
+        let currentBlockNumber = startBlockNumber
+        let currentBlock = await provider.getBlock(currentBlockNumber);
+
+        while (currentBlock.timestamp > timestamp) {
+            let timeDiffrence = currentBlock.timestamp - timestamp
+            const blockStepBack = Math.ceil(timeDiffrence / averageBlockTime); // Approximately 12 seconds per block
+            if (blockStepBack < 1) break;
+            // reduce the current block number by the block step back
+            currentBlockNumber -= blockStepBack
+            currentBlock = await provider.getBlock(currentBlockNumber);
+        }
+
+        // If we back step too low
+        if (currentBlock.timestamp < timestamp) {
+            // advance one block at a time 
+            while (currentBlock.timestamp < timestamp) {
+                currentBlockNumber += 1
+                if (currentBlockNumber > startBlockNumber) break;
+                currentBlock = await provider.getBlock(currentBlockNumber);
+            }
+        }
+
+        return currentBlock
+
     } catch (error) {
         console.log("The error :", error)
-        reject(error)
+
     }
-})
+}
 
